a6652d805584713bbea2f63109fbc318f024cd7f
watarikai96 <watarikai@outlook.com>
2025-03-14 21:10:30 +0900

    CSS Fundamentals Practice Session

--- Front End Practice/JavaScript/script.js
+++ Front End Practice/JavaScript/script.js
@@ -4,13 +4,15 @@
 // Variables
 // let b;
 // var c;
-const a = 10;
-const b = 20;
-const c = 30;
-const d = " text";
-const e = "Hello "
-// const d = a + b + c;
-//
-// console.log(d);
-const sum = a + b - c;
-console.log(e + a + sum + a + d);
+// const a = 10;
+// const b = 20;
+// const c = 30;
+// const d = " text";
+// const e = "Hello "
+// // const d = a + b + c;
+// //
+// // console.log(d);
+// const sum = a + b - c;
+// console.log(e + a + sum + a + d);
+
+
